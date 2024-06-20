49d6030d37accc997d6f8da50e016ba14ad08532
shnuk2 <nasariykos@gmail.com>
2024-06-20 12:54:53 +0300

    updated setings.py

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -113,7 +113,6 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'static/assets')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
